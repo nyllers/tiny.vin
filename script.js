c0996cb7967d90aeccfa88c3aba3302abd423fba
nyllers <anders.nyllet.nylund@gmail.com>
2026-07-25 14:40:02 +0200

    Add random URL generator to homepage (#2)
    * Add random URL generator to homepage
    
    * Move generated URL to a separate result area

---
+++ script.js
@@ -0,0 +1,14 @@
+const CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+function generateUrl() {
+  let code = "";
+  for (let i = 0; i < 12; i++) {
+    code += CHARS[Math.floor(Math.random() * CHARS.length)];
+  }
+  document.getElementById("url-result").textContent = `https://tiny.vin/${code}`;
+}
+
+document.getElementById("generate-btn").addEventListener("click", generateUrl);
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") generateUrl();
+});
